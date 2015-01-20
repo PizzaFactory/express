668de7544d33b86b5f3f6d6d12c7d809359726b3
Masaki Muranaka <monaka@monami-ya.com>
2015-01-20 13:46:39 +0900

    Prepare for release.

---
+++ release.sh
@@ -0,0 +1,19 @@
+versionBase=8.5.0
+newVersion=${versionBase}.$(TZ=UTC date +M%Y%m%d-%H%M)
+#!/bin/bash
+
+set -eu
+
+git flow release start $newVersion
+
+mvn -f parent/pom.xml tycho-versions:set-version -DnewVersion=$newVersion
+git add .
+git commit -m 'Prepare for release.' 
+git flow release -s finish $newVersion
+
+mvn -f parent/pom.xml tycho-versions:set-version -DnewVersion=${versionBase}.qualifier
+git add .
+git commit -m 'Back to snapshots.'
+
+git push --all
+git push --tags
